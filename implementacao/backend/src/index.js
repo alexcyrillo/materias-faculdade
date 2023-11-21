5f98109e4f408377bfe30211925cd6f6f7a356a5
Lucas de Castro <lucasnizio23@gmail.com>
2023-11-20 21:23:53 -0300

    fix: configurando CORS

--- implementacao/backend/src/index.js
+++ implementacao/backend/src/index.js
@@ -1,11 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import routes from "./routes.js";
 
 dotenv.config();
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
