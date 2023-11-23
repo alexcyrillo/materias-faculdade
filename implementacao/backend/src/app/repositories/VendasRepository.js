b3083aca4d4f9d19cb81af83d04fee929d4c229f
Lucas Nizio <lucasnizio23@gmail.com>
2023-11-23 11:36:32 -0300

    fix: retirando console.log

--- implementacao/backend/src/app/repositories/VendasRepository.js
+++ implementacao/backend/src/app/repositories/VendasRepository.js
@@ -5,7 +5,6 @@ class VendasRepository {
 		let sql = "SELECT qt_vendas FROM funcionarios WHERE cpf=?";
 		const qtVendas = (await consulta(sql, [id]))[0];
 		qtVendas.qt_vendas++;
-		console.log(qtVendas);
 		sql = "UPDATE funcionarios SET ? WHERE cpf=?";
 		consulta(sql, [qtVendas, id]);
 	}
@@ -14,7 +13,6 @@ class VendasRepository {
 		let sql = "SELECT qt_compras FROM clientes WHERE cpf=?";
 		const qtVendas = (await consulta(sql, [id]))[0];
 		qtVendas.qt_compras++;
-		console.log(qtVendas);
 		sql = "UPDATE clientes SET ? WHERE cpf=?";
 		consulta(sql, [qtVendas, id]);
 	}
@@ -23,7 +21,6 @@ class VendasRepository {
 		let sql = "SELECT quantidade FROM produtos WHERE id=?";
 		const qtVendas = (await consulta(sql, [id]))[0];
 		qtVendas.quantidade--;
-		console.log(qtVendas);
 		sql = "UPDATE produtos SET ? WHERE id=?";
 		consulta(sql, [qtVendas, id]);
 	}
